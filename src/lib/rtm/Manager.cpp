128eed328bd80741cef87e5dc2342dd19d0a91bd
Fumio KANEHIRO <fkanehiro@gmail.com>
2019-01-26 09:33:13 +0900

    disable automatic module loading

--- src/lib/rtm/Manager.cpp
+++ src/lib/rtm/Manager.cpp
@@ -691,7 +691,8 @@ std::vector<coil::Properties> Manager::getLoadableModules()
       {
         RTC_ERROR(("Factory not found: %s",
                    comp_id["implementation_id"].c_str()));
-        
+
+        return 0;
         // automatic module loading
         std::vector<coil::Properties> mp(m_module->getLoadableModules());
         RTC_INFO(("%d loadable modules found", mp.size()));
